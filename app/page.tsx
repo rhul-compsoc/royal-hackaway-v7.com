9f17e269ac8c2e743cc45a4b02204de4aa83ae77
Jacob Padley <jacob5180@hotmail.co.uk>
2023-11-01 11:29:43 +0000

    Add responsive placeholder V7 logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import logo from './v7-logo.png'
+import background from './background.png'
+
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <Image alt="Hackaway Photo"
+        src={background}
+        placeholder="blur"
+        fill
+        className="object-cover blur brightness-50" />
+      <div className="w-full pt-10">
+        <Image alt="Hackaway V7 Logo" src={logo} className="blur-none drop-shadow-md mx-auto lg:w-1/2 w-3/4" />
+      </div>
+    </main>
   )
 }
