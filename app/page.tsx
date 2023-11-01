51893b53e71c94e6287eeb8821083253e312b1c8
joseph <joebeck03@icloud.com>
2023-11-01 12:36:38 +0000

    feat: add footer & font awesome

--- app/page.tsx
+++ app/page.tsx
@@ -2,44 +2,49 @@
 
 import { Accordion } from "@/components/accordion";
 import { Button } from "@/components/button";
+import { Footer } from "@/components/footer";
 import { Link } from "@/components/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Accordion
-        elements={[
-          {
-            title: "Lorem ipsum dolor sit amet consectetur adipisicing?",
-            body: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-            molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-            voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-            earum similique!`
-          },
-          {
-            title: "Lorem ipsum dolor sit amet consectetur?",
-            body: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-            molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-            voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-            earum similique!`
-          },
-        ]}
-      />
+    <main className="">
+      <div className="flex flex-col min-h-screen items-center justify-between p-24">
+        <Accordion
+          elements={[
+            {
+              title: "Lorem ipsum dolor sit amet consectetur adipisicing?",
+              body: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+              molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+              voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+              earum similique!`
+            },
+            {
+              title: "Lorem ipsum dolor sit amet consectetur?",
+              body: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+              molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+              voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+              earum similique!`
+            },
+          ]}
+        />
 
-      <Button
-        text="Click Me!"
-        on_click={ () => {
-            let res = prompt("button pressed");
-            console.log(res);
+        <Button
+          text="Click Me!"
+          on_click={ () => {
+              let res = prompt("button pressed");
+              console.log(res);
+            }
           }
-        }
-      />
+        />
 
-      <Link
-        text="Link me!"
-        href="https://www.cmpsc.uk"
-        target="_blank"
-      />
+        <Link
+          text="Link me!"
+          href="https://www.cmpsc.uk"
+          target="_blank"
+        />
+      </div>
+
+      <Footer/>
     </main>
   );
 }
