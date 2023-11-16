37cfb7f889990fd0aa6650e3e4d4d4ddf37440f8
Jacob Padley <jacob5180@hotmail.co.uk>
2023-11-16 12:40:08 +0000

    Make background image stay still when scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,17 @@ import { Card } from '@/components/card';
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
+      <div className="absolute h-screen w-full [clip-path:inset(0,0,0,0)]">
+        <div className="fixed w-full h-full left-0 top-0">
+          <Image alt="Hackaway Photo"
+            src={background}
+            placeholder="blur"
+            fill
+            className="object-cover blur brightness-50" />
+        </div>
+      </div>
       <div className="flex flex-col min-h-screen items-center justify-between p-24">
-        <Image alt="Hackaway Photo"
-          src={background}
-          placeholder="blur"
-          fill
-          className="object-cover blur brightness-50" />
         <div className="w-full pt-10">
           <Image alt="Hackaway V7 Logo" src={logo} className="blur-none drop-shadow-[4px_4px_rgba(217,91,23,.75)] mx-auto lg:w-1/2 w-3/4" />
         </div>
@@ -31,91 +35,93 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col min-h-screen items-center justify-between p-24">
-        <Accordion
-          elements={[
-            {
-              title: "Lorem ipsum dolor sit amet consectetur adipisicing?",
-              body: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-              molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-              voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-              earum similique!`
-            },
-            {
-              title: "Lorem ipsum dolor sit amet consectetur?",
-              body: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-              molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-              voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-              earum similique!`
-            },
-          ]}
-        />
+      <div className="relative w-full h-full bg-white">
+        <div className="relative flex flex-col min-h-screen items-center justify-between p-24">
+          <Accordion
+            elements={[
+              {
+                title: "Lorem ipsum dolor sit amet consectetur adipisicing?",
+                body: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+                molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+                voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+                earum similique!`
+              },
+              {
+                title: "Lorem ipsum dolor sit amet consectetur?",
+                body: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+                molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+                voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+                earum similique!`
+              },
+            ]}
+          />
 
-        <Button
-          text="Click Me!"
-          on_click={ () => {
-              let res = prompt("button pressed");
-              console.log(res);
+          <Button
+            text="Click Me!"
+            on_click={ () => {
+                let res = prompt("button pressed");
+                console.log(res);
+              }
             }
-          }
-        />
-
-        <Link
-          text="Link me!"
-          href="https://www.cmpsc.uk"
-          target="_blank"
-        />
-
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
-          <Card
-            name='Joseph'
-            position='Treasurer'
-            img='joseph.jpg'
-            alt='joseph'
           />
 
-          <Card
-            name='Joseph'
-            position='Treasurer'
-            img='joseph.jpg'
-            alt='joseph'
+          <Link
+            text="Link me!"
+            href="https://www.cmpsc.uk"
+            target="_blank"
           />
 
-          <Card
-            name='Joseph'
-            position='Treasurer'
-            img='joseph.jpg'
-            alt='joseph'
-          />
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+            <Card
+              name='Joseph'
+              position='Treasurer'
+              img='joseph.jpg'
+              alt='joseph'
+            />
 
-          <Card
-            name='Joseph'
-            position='Treasurer'
-            img='joseph.jpg'
-            alt='joseph'
+            <Card
+              name='Joseph'
+              position='Treasurer'
+              img='joseph.jpg'
+              alt='joseph'
+            />
+
+            <Card
+              name='Joseph'
+              position='Treasurer'
+              img='joseph.jpg'
+              alt='joseph'
+            />
+
+            <Card
+              name='Joseph'
+              position='Treasurer'
+              img='joseph.jpg'
+              alt='joseph'
+            />
+          </div>
+
+        </div>
+
+        <div className="relative flex-col min-h-screen items-center justify-between p-96">
+          <Schedule
+            events={[
+              {
+                time: "17:00",
+                title: "Dinner",
+                description: "Eat some food! Probably just gonna be a tonne of pizza or something like that."
+              },
+              {
+                time: "20:00",
+                title: "Carrot in a Box",
+                description: "A long standing tradition! Compete to become the highly respected Carrot in a Box champion."
+              },
+            ]}
           />
         </div>
 
+        <Footer/>
       </div>
-
-      <div className="flex-col min-h-screen items-center justify-between p-96">
-        <Schedule
-          events={[
-            {
-              time: "17:00",
-              title: "Dinner",
-              description: "Eat some food! Probably just gonna be a tonne of pizza or something like that."
-            },
-            {
-              time: "20:00",
-              title: "Carrot in a Box",
-              description: "A long standing tradition! Compete to become the highly respected Carrot in a Box champion."
-            },
-          ]}
-        />
-      </div>
-
-      <Footer/>
     </main>
   );
 }
