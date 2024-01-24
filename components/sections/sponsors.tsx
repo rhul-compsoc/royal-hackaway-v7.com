0c665acff829dd966402b474a9ae2ff0f460a3fb
Zakariya <mzkhaled468@gmail.com>
2024-01-24 15:01:38 +0000

    added isg as sponsor - all done!

--- components/sections/sponsors.tsx
+++ components/sections/sponsors.tsx
@@ -55,9 +55,7 @@ export const Sponsors = (): JSX.Element => {
         <div className="flex flex-col items-center text-center">
           <H3 text="Bronze"></H3>
           <div
-            className={`grid ${
-              true ? "justify-center" : "grid-cols-1 lg:grid-cols-3"
-            } gap-4 lg:gap-8 mx-auto place-content-center`}
+            className={`grid grid-cols-2 lg:grid-cols-2 gap-4 lg:gap-8 mx-auto place-content-center`}
           >
             <Sponsor
               name="BCS"
